78764c4ef5c6e2fc6b72cd54cbf1472e775dee39
Timofei Macohin <good.men.tim@gmail.com>
2023-03-28 19:00:59 +0300

    test+convert

--- iternal/s_reg-ident/str/regout/regout.go
+++ iternal/s_reg-ident/str/regout/regout.go
@@ -1,6 +1,8 @@
 package regout
 
 import (
+	"pet/pkg/convert"
+
 	"pet/iternal/s_reg-ident/str/regin"
 	"pet/iternal/s_reg-ident/str/salt"
 )
@@ -14,17 +16,13 @@ type RegDataDB struct {
 	saltdynamicBt   []byte
 }
 
-func NewInToOut(salt *salt.Salt, rdin *regin.RegDataIn, key []byte) (rdout *RegDataDB) {
-	var intermediateint []int
-	var intermediatestr []string
+func NewReg(salt *salt.Salt, rdin *regin.RegDataIn, key []byte) (rdout *RegDataDB) {
 
 	rdout.loginname, rdout.email = rdin.GetRDIn()
 	rdout.passwordhashBt = key
 	rdout.saltdynamicBt = salt.GetDyanmicSalt()
-
-	for i := 0; i < len(rdout.passwordhashBt); i++ {
-
-	}
+	rdout.passwordhashStr = convert.IntToStr(convert.ByteToInt(rdout.passwordhashBt))
+	rdout.saltdynamicStr = convert.IntToStr(convert.ByteToInt(rdout.saltdynamicBt))
 
 	return rdout
 }
