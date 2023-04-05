223658a5a61e55ded460e9f50a0985bdc522db36
Timofei Macohin <good.men.tim@gmail.com>
2023-04-05 18:11:57 +0300

    start of authorization implementation

--- iternal/s_reg-ident/str/auth/auth.go
+++ iternal/s_reg-ident/str/auth/auth.go
@@ -1,12 +1,20 @@
 package auth
 
 import (
+	"pet/iternal/s_reg-ident/db/comsql"
 	"pet/iternal/s_reg-ident/str/salt"
 )
 
 type Auth struct {
+	id       int
 	logname  string
 	password string
 	salt     *salt.Salt
 	key      string
+}
+
+func New(account *comsql.Account) (a *Auth) {
+	a.id = account.Id
+	a.key = account.Key
+	return a
 }
