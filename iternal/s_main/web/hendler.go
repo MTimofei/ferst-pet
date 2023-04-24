c65e8ef391d71ce6afc4030c221738ce6c02a044
Timofei Macohin <good.men.tim@gmail.com>
2023-04-24 16:04:36 +0300

    mini fix

--- iternal/s_main/web/hendler.go
+++ iternal/s_main/web/hendler.go
@@ -10,8 +10,11 @@ import (
 
 func (con *Connect) hendlerMain(w http.ResponseWriter, r *http.Request) {
 	token, err := cookie.HandlingCookiesClients(con.UrlServer, con.PageKesh, con.PublicKey, w, r)
-
-	err = pars.ParsPage(w, "hi", con.PageKesh, str.NewAccountFromJWT(token))
+	if err != nil {
+		myerr.ServesError(w, con.PageKesh, err)
+		return
+	}
+	err = pars.ParsPage(w, "hi", con.PageKesh, str.NewAccountFromJWT(token).Name)
 	if err != nil {
 		myerr.ServesError(w, con.PageKesh, err)
 		return
